997bbff2539dc55e8746424c058a00b685c32a26
ogbodo philip <mrogbodophilip@gmail.com>
2026-01-18 05:54:03 -0800

    new

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,6 +1,20 @@
-import { getAuthConfigProvider } from "@convex-dev/better-auth/auth-config";
+// import { getAuthConfigProvider } from "@convex-dev/better-auth/auth-config";
+// import type { AuthConfig } from "convex/server";
+
+// export default {
+//   providers: [getAuthConfigProvider()],
+// } satisfies AuthConfig;
+
+
+
 import type { AuthConfig } from "convex/server";
 
 export default {
-  providers: [getAuthConfigProvider()],
+  providers: [
+    {
+      // This forces Convex to trust the Vercel URL specifically
+      domain: "https://bloom-eight-kappa.vercel.app",
+      applicationID: "convex",
+    },
+  ],
 } satisfies AuthConfig;
